3965c65ec4751e32c71ef1d4acce36469d4331ab
Ben Sapirstein <ben.sap@samsung.com>
2022-09-06 10:41:29 +0300

    Implemented configuration loading from config.json

--- src/GenerativeType.py
+++ src/GenerativeType.py
@@ -15,67 +15,61 @@ SEED_MAX = 2**32 - 1
 
 class GenerativeType():
 
-    def __init__(self, url, name, outdir, R, r, offset_stroke=None):
-        #base_svg = download(url)
-        svg_path = url
-        # Radius of spectrum
-        self.R = R
-        # radius around sample
-        self.r = r
-        self.name = name
-        self.outdir = outdir
-        self.word = SpectrumWord(svg_path, offset_stroke)
+    def __init__(self, config):
+        # Offset stroke
+        os_color = np.asarray(config["os_color"])/ 255
+        os_width = config["os_width"]
+        offset_stroke = (os_color, os_width) if os_width > 0 else None
+
+        self.word = SpectrumWord(config["svg_path"], offset_stroke)
         self.l_count = len(self.word.letters)
+
+        self.frames_count = config["frames"]
         if self.word.forms_count == 2:
             self.dist = "uniform"
-            self.frames = 10
+            self.frames_count //= 2
             self.pad = 0
         elif self.word.forms_count == 4:
             self.dist = "normal"
-            self.frames = 20
         else:
             raise Exception("GenerativeType supports only 2 or 4 forms")
 
+        # Radius of spectrum, radius around sample
+        self.R, self.r = config["R"], config["r"]
 
-    def generate_animation(self, id, seed=None, transparent=True):
+        self.transparency = config["transparency"]
+
+        self.name = config["name"]
+        self.outdir = f"../Results/{self.name}"
+        Path(self.outdir).mkdir(parents=True, exist_ok=True)
+
+
+    def generate(self, id, seed=None):
         np.random.seed(seed)
         C = sample_centers(self.word.forms_count, self.l_count, self.dist, self.R, self.r)
         print_drawing(self.word, C, f"{self.name}_{id}", self.outdir)
         if self.word.forms_count == 2:
-            samples = sample_straight_animation(self.l_count, self.frames, self.pad, C, self.r)
+            samples = sample_straight_animation(self.l_count, self.frames_count, self.pad, C, self.r)
 
         elif self.word.forms_count == 4:
-            samples = sample_cirular_animation(C, self.l_count, 20, self.r)
+            samples = sample_cirular_animation(C, self.l_count, self.frames_count, self.r)
 
-        animate_drawing(self.word, samples, f"{self.name}_{id}", self.outdir, transparent=transparent)
+        animate_drawing(self.word, samples, f"{self.name}_{id}", self.outdir, transparent=self.transparency)
         return C
 
-    def generate_image(self, id, seed=None):
-        np.random.seed(seed)
-        C = sample_centers(self.word.forms_count, self.l_count, self.dist, self.R, self.r)
-
-        print_drawing(self.word, C, self.name, self.outdir)
-
-        return C
         # metadata = {"strokewidth" : None,
         #             "pallete" = cmap[i],
         #             "svg" = }
 
 def main():
     configuration_path = "config.json"
-    randomness_path = "randomness.json"
-    input_path = "../Art/World.svg"
-    name = "World"
-    outdir = f"../Results/{name}"
-    Path(outdir).mkdir(parents=True, exist_ok=True)
+    config = json.load(open(configuration_path))
 
-    offset_stroke = ((0, 139/255, 68/255), 5)
-    transparency = False
-    nft = GenerativeType(input_path, name, outdir, 1, 0.1, None)
+    nft = GenerativeType(config)
 
-    seed_data = json.load(open(randomness_path))
+    seed_data = json.load(open(config["randomness_path"]))
     for entry in seed_data:
-        nft.generate_animation(entry["id"], entry["randomness"] % SEED_MAX, transparency)
+        nft.generate(entry["id"], entry["randomness"] % SEED_MAX)
 
 if "__main__" == __name__:
     main()
