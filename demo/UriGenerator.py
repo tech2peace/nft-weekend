7581e1112676ca623f429b0ef27cefdcf7c281d2
y-khawa <khawa@aljaliya.io>
2022-09-10 15:44:29 +0300

    MAL and EGG files

--- demo/UriGenerator.py
+++ demo/UriGenerator.py
@@ -70,7 +70,7 @@ class UriGenerator:
         output_uris = []
         id = 0
 
-        while os.path.isfile(os.environ.get('IMAGE_PREFIX') + str(id) + os.environ.get('IMAGE_FORMAT')):
+        while os.path.isfile(os.path.join(results_dir, os.environ.get('IMAGE_PREFIX') + str(id) + os.environ.get('IMAGE_FORMAT'))):
             metadata_filename = os.environ.get('METADATA_PREFIX') + str(id) + '.json'
             image_filename = os.environ.get('IMAGE_PREFIX') + str(id) + os.environ.get('IMAGE_FORMAT')
             
@@ -81,15 +81,15 @@ class UriGenerator:
             # upload image
             print("uploading image for token " + str(id) + " to ipfs...")
             image_cid = self.pinata.pin_file_to_ipfs(image_filename)
-            print("complete")
+            print("uploaded at ipfs://" + image_cid["IpfsHash"])
 
             # fetch metadata
             metadata_file = open(metadata_filename)
             metadata_json = json.load(metadata_file)
+            metadata_file.close()
 
             # append the image uri
             metadata_json.update({"image": "ipfs://" + image_cid["IpfsHash"]})
-            metadata_file.close()
 
             # write metadata back
             with open(metadata_filename, "w") as out_file:
@@ -99,7 +99,7 @@ class UriGenerator:
             # upload metadata
             print("uploading metadata for token " + str(id) + " to ipfs...")
             metadata_cid = self.pinata.pin_file_to_ipfs(metadata_filename)
-            print("complete")
+            print("uploaded at ipfs://" + metadata_cid["IpfsHash"])
 
             # add metadata uri to output
             output_uris.append({"id": id, "uri": "ipfs://" + metadata_cid["IpfsHash"]})
