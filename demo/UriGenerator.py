56da1162dc6b7508999694b96d064a5760ff6d1f
y-khawa <khawa@aljaliya.io>
2022-09-10 10:12:23 +0300

    script fix

--- demo/UriGenerator.py
+++ demo/UriGenerator.py
@@ -16,7 +16,7 @@ class UriGenerator:
     def __init__(self, address_book = "./contracts/address_book.json"):
         print('connecting to Infura...')
         self.web3 = Web3(Web3.HTTPProvider(os.getenv('INFURA_URL')))
-        self.contracts = json.load(open(address_book)
+        self.contracts = json.load(open(address_book))
 
         print('connecting to Pinata...')
         self.pinata = PinataPy(str(os.environ.get('PINATA_API_KEY')), str(os.environ.get('PINATA_API_SECRET')))
@@ -38,7 +38,7 @@ class UriGenerator:
             # write uris to output file
             
             print("writing output file...")
-            with open(contract_path + "results/uris.json", "w") as out_file:
+            with open(contract_path + "output/uris.json", "w") as out_file:
                 out_file.write(json.dumps(uris, indent = 4))
 
     def readRandomnessFromContract(self, contract):
@@ -70,43 +70,45 @@ class UriGenerator:
         output_uris = []
         id = 0
 
-        # copy files to directory           
-        file_names = os.listdir(results_dir)
-        for file_name in file_names:
-            shutil.move(os.path.join(results_dir, file_name), ".")
-
         while os.path.isfile(os.environ.get('IMAGE_PREFIX') + str(id) + os.environ.get('IMAGE_FORMAT')):
-
+            metadata_filename = os.environ.get('METADATA_PREFIX') + str(id) + '.json'
+            image_filename = os.environ.get('IMAGE_PREFIX') + str(id) + os.environ.get('IMAGE_FORMAT')
+            
+            # move files to directory           
+            shutil.move(os.path.join(results_dir, metadata_filename), ".")
+            shutil.move(os.path.join(results_dir, image_filename), ".")
+            
             # upload image
             print("uploading image for token " + str(id) + " to ipfs...")
-            image_cid = self.pinata.pin_file_to_ipfs(os.environ.get('IMAGE_PREFIX') + str(id) + os.environ.get('IMAGE_FORMAT'))
+            image_cid = self.pinata.pin_file_to_ipfs(image_filename)
             print("complete")
 
             # fetch metadata
-            metadata_file = open(os.environ.get('METADATA_PREFIX') + str(id) + '.json')
+            metadata_file = open(metadata_filename)
             metadata_json = json.load(metadata_file)
 
             # append the image uri
             metadata_json.update({"image": "ipfs://" + image_cid["IpfsHash"]})
+            metadata_file.close()
 
             # write metadata back
-            with open(os.environ.get('METADATA_PREFIX') + str(id) + '.json', "w") as out_file:
+            with open(metadata_filename, "w") as out_file:
                 out_file.write(json.dumps(metadata_json, indent = 4))
+                out_file.close()
             
             # upload metadata
             print("uploading metadata for token " + str(id) + " to ipfs...")
-            metadata_cid = self.pinata.pin_file_to_ipfs(os.environ.get('METADATA_PREFIX') + str(id) + ".json")
+            metadata_cid = self.pinata.pin_file_to_ipfs(metadata_filename)
             print("complete")
 
             # add metadata uri to output
             output_uris.append({"id": id, "uri": "ipfs://" + metadata_cid["IpfsHash"]})
+            
+            # move files back
+            shutil.move(metadata_filename, results_dir)
+            shutil.move(image_filename, results_dir)
 
-            id += 1
-
-            out_file.close()
-
-        #for file_name in file_names:
-        #    shutil.move(file_name, results_dir)
+            id += 1          
 
         return output_uris
 
